0bac3e8a2436e519c48f2aeedf538e7f8b00062c
Stanislav Mitev <stanitev@gmail.com>
2023-03-27 20:04:17 +0300

    add a collision between the two objects

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 # import time
@@ -11,8 +12,11 @@ screen.bgcolor("black")
 screen.title("My Snake Game!")
 screen.tracer(0)
 
+# Create the objects
 snake = Snake()
+food = Food()
 
+# Create a movement
 screen.listen()
 screen.onkey(fun=snake.move_up, key="Up")
 screen.onkey(fun=snake.move_down, key="Down")
@@ -21,6 +25,7 @@ screen.onkey(fun=snake.move_right, key="Right")
 
 # Variables
 game_is_on = True
+collision_distance = 15
 
 # Create the snake
 
@@ -29,5 +34,9 @@ while game_is_on:
     screen.update()  # Updates the screen when the for loop is over
     time.sleep(0.1)  # Add a delay in the movement
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < collision_distance:
+        food.refresh()
 
 screen.exitonclick()
