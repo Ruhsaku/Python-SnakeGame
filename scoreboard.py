210ac36704c4013788068fbb18acffad7d88d2d1
Stanislav Mitev <stanitev@gmail.com>
2023-03-31 19:59:24 +0300

    adding high score variable and reset method for the score

--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,5 @@
 from turtle import Turtle
 
-TEXT_X_POSITION = 0
-TEXT_Y_POSITION = 260
 ALIGNMENT = "center"
 FONT = ('Courier', 24, 'normal')
 
@@ -10,21 +8,27 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
-        self.goto(x=TEXT_X_POSITION, y=TEXT_Y_POSITION)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.goto(0, 260)
         self.hideturtle()
+        self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over.", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
